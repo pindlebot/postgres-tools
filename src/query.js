70b96bfd2479393269892b882001d0cf6bd3f124
Ben <menubar@protonmail.com>
2018-09-18 10:18:21 -0400

    new

--- src/query.js
+++ src/query.js
@@ -2,7 +2,8 @@ const checkout = require('./checkout')
 const format = require('./format')
 
 const query = async (...args) => {
-  const client = await checkout()
+  const options = args.find(arg => typeof arg === 'object' && !Array.isArray(arg))
+  const client = await checkout(options || {})
   const data = await client.query(...args)
     .catch(error => {
       throw error
